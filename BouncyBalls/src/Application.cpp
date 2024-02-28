ed2a347709abc50554d4a3c9eb386fca84f2f28e
Constantin-Marius Costescu <101038739+CostescuMarius@users.noreply.github.com>
2024-02-28 13:55:08 +0200

    Prezentare 2 - setare perspectiva si efect scroll camera

--- BouncyBalls/src/Application.cpp
+++ BouncyBalls/src/Application.cpp
@@ -9,18 +9,19 @@ const int WINDOW_WIDTH = 1800;
 const int WINDOW_HEIGHT = 900;
 
 bool rightClickPressed = false;
+float cameraPositionZ = -3.0f;
 double lastX = WINDOW_WIDTH / 2.0;
 double lastY = WINDOW_HEIGHT / 2.0;
 float yaw = -90.0f;
 float pitch = 0.0f;
 
-void processInput(GLFWwindow* window);
+void setCameraPerspective(GLFWwindow* window);
+
+void drawSphere();
+
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 int main(void)
 {
@@ -41,33 +42,17 @@ int main(void)
 
     glfwSetMouseButtonCallback(window, mouse_button_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
-    //glfwSetScrollCallback(window, scroll_callback);
+    glfwSetScrollCallback(window, scroll_callback);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
 
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        processInput(window);
-
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef(pitch, 1.0f, 0.0f, 0.0f);
-        glRotatef(yaw, 0.0f, 1.0f, 0.0f);
-
-
-
-        GLUquadric* quad = gluNewQuadric();
-        gluQuadricDrawStyle(quad, GLU_LINE);
-
-        glColor3f(1.0f, 0.0f, 0.0f);
-        gluSphere(quad, 0.2, 40, 20);
-        gluDeleteQuadric(quad);
-
+        setCameraPerspective(window);
+        
+        drawSphere();
 
         glfwSwapBuffers(window);
 
@@ -78,14 +63,28 @@ int main(void)
     return 0;
 }
 
-// ESC closes windows
-void processInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-    }
+void setCameraPerspective(GLFWwindow* window) {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    int sphereWidth;
+    int sphereHeight;
+    glfwGetFramebufferSize(window, &sphereWidth, &sphereHeight);
+    gluPerspective(45.0f, (float)sphereWidth / (float)sphereHeight, 0.1f, 100.0f);
+
+    glTranslatef(0.0f, 0.0f, cameraPositionZ);
+
+    glRotatef(pitch, 1.0f, 0.0f, 0.0f);
+    glRotatef(yaw, 0.0f, 1.0f, 0.0f);
 }
 
+void drawSphere() {
+    GLUquadric* quad = gluNewQuadric();
+    gluQuadricDrawStyle(quad, GLU_LINE);
+
+    glColor3f(1.0f, 0.0f, 0.0f);
+    gluSphere(quad, 0.3, 40, 40);
+    gluDeleteQuadric(quad);
+}
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
@@ -117,4 +116,12 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
     {
         rightClickPressed = false;
     }
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    const float scrollSensitivity = 0.5f;
+
+
+    cameraPositionZ += yoffset * scrollSensitivity;
 }
